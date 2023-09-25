c9b199a9b637993ade0065751768031233fb70c9
nagaminehiro <hiro.nagamini@outlook.com>
2023-09-25 00:03:01 -0300

    conexoes confirmação
    conexoes confirmação

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const initConfigs = require('./configs/init');
 const getCorsOptions = require('./configs/cors');
 const messageRouter = require('./routes/messages.route');
+const attendanceRouter = require('./routes/attendance.route');
 
 const app = express();
 const PORT = 3000;
@@ -20,6 +21,7 @@ initConfigs();
 
 // Rotas
 app.use('/message', messageRouter);
+app.use('/attendance', attendanceRouter);
 
 // Inicie o servidor
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
